13f8363c9b806549454e5f4f013b3a1157689d14
nico <gea.cossou.nico@gmail.com>
2021-01-21 09:48:33 -0500

    first commit

---
+++ src/screens/Signin.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Signin = () => (
+    <div>
+        <h2>hi</h2>
+    </div>
+);
+
+export default Signin;
---
+++ src/screens/Topic.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Topic = () => (
+    <div>
+        <h2>Topic</h2>
+    </div>
+);
+
+export default Topic;
